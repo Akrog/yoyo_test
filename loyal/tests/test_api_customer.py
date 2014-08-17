a73a1f9812b78dab5537056b34152fe58aeb7e4f
akrog <gorka@eguileor.com>
2014-08-17 16:17:23 +0100

    Added test for customer detail with counters

--- loyal/tests/test_api_customer.py
+++ loyal/tests/test_api_customer.py
@@ -2,8 +2,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
-from loyal.models import Customer
+from loyal.models import Customer, Product, Sale, Stamp, Voucher
 
 
 class APICustomer(APITestCase):
@@ -11,6 +12,13 @@ class APICustomer(APITestCase):
     new_customer = {'first_name':"John",
                     'last_name':"Doe",
                     'email': "john.doe@gmail.com"}
+
+    new_product = {
+        'kind': Product.WIDGET,
+        'date': timezone.now(),
+        'serial_num': "1",
+        'sale': None
+    }
 
     def test_get_list_empty(self):
         """
@@ -90,6 +98,48 @@ class APICustomer(APITestCase):
         expected_customer['num_stamps'] = 0
         expected_customer['num_vouchers'] = 0
         expected_customer['num_purchases'] = 0
+
+        # Confirm it's OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(expected_customer, response.data)
+
+
+    def test_get_detail_one_customer_with_counters(self):
+        """
+        Verify that the customers detail endpoint returns customer info.
+        This is a basic test with only 1 customer but wih data on the
+        counters.
+        """
+
+        num_sales = 3
+        num_vouchers = 2
+
+        # Create customer
+        c = Customer(**self.new_customer)
+        c.save()
+
+        for _ in xrange(num_sales):
+            s = Sale(customer=c, date=timezone.now())
+            s.save()
+
+        p = Product(**self.new_product)
+        p.save()
+
+        st = Stamp(owned_by=c, obtained_with=p, grouped_in=None)
+        st.save()
+
+        for _ in xrange(num_vouchers):
+            v = Voucher(owned_by=c, redeemed_with=None)
+            v.save()
+
+        # Get details
+        url = reverse('loyal:customer-detail', args=[c.pk])
+        response = self.client.get(url)
+
+        expected_customer = dict(self.new_customer)
+        expected_customer['num_stamps'] = 1
+        expected_customer['num_vouchers'] = 2
+        expected_customer['num_purchases'] = 3
 
         # Confirm it's OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
