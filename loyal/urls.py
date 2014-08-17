4b0d7acdde22a0bd05bbd9820cacae8b849d48a9
akrog <gorka@eguileor.com>
2014-08-18 00:31:28 +0100

    Making it a little bit more RESTful. Now from root you'll get links to admin, docs and loyal and from loyal you'll get a link to customer

--- loyal/urls.py
+++ loyal/urls.py
@@ -2,6 +2,19 @@ from django.conf.urls import patterns, url, include
 
 from loyal.views import CustomerList, CustomerDetail, StampList
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(('GET',))
+def api_loyal(request, format=None):
+    return Response({
+        'customer': reverse('loyal:customer:customer-list', request=request, format=format),
+    })
+
+
 urlpatterns = patterns('',
+                       url(r'^$', api_loyal, name="root"),
                        url(r'^customer/', include('loyal.urls_customer', namespace='customer'))
 )
--- yoyo_test/urls.py
+++ yoyo_test/urls.py
@@ -1,10 +1,24 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'loyal': reverse('loyal:root', request=request, format=format),
+        'admin': reverse('admin:index', request=request, format=format),
+        'docs': reverse('django.swagger.base.view', request=request, format=format),
+    })
 
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
+    url(r'^$', api_root),
     url(r'^loyal/', include('loyal.urls', namespace='loyal')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
