ed3e5a2bfb242a0d2c323b0b6f3eb28fe765bfe9
akrog <gorka@eguileor.com>
2014-08-17 10:37:23 +0100

    loyal app created

--- yoyo_test/settings.py
+++ yoyo_test/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loyal',
 )
 
 MIDDLEWARE_CLASSES = (
--- yoyo_test/urls.py
+++ yoyo_test/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'yoyo_test.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^/loyal/', include(loyal.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
