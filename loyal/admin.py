7ff1ee81fabee6bca4facaf3cb877f40c786a73c
akrog <gorka@eguileor.com>
2014-08-17 15:30:55 +0100

    Added stamp and voucher to admin interface

--- loyal/admin.py
+++ loyal/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from loyal.models import Customer, Sale, Product
+from loyal.models import Customer, Sale, Product, Stamp, Voucher
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Sale)
 admin.site.register(Product)
+admin.site.register(Stamp)
+admin.site.register(Voucher)
