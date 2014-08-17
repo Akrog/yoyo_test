89eb5798668c10e4f39bed5d4004a589e24e19c5
akrog <gorka@eguileor.com>
2014-08-17 10:25:02 +0100

    Language and Timezone settings

--- yoyo_test/settings.py
+++ yoyo_test/settings.py
@@ -65,9 +65,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'WET'
 
 USE_I18N = True
 
