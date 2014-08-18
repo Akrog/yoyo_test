e2c047bc926ebcf9afd7cc8f33414233cb957be9
akrog <gorka@eguileor.com>
2014-08-18 01:55:01 +0100

    Added list of products on sale endpoint

--- loyal/serializers/sale_serializer.py
+++ loyal/serializers/sale_serializer.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from loyal.models import Sale, Customer
 
 class SaleSerializer(serializers.ModelSerializer):
+    products = serializers.RelatedField(source="product_set", many=True)
+
     class Meta:
         model = Sale
-        fields = ('customer', 'date')
+        fields = ('customer', 'date', 'products')
+
 
     def restore_object(self, attrs, instance=None):
         pk = self.context['view'].kwargs['pk']
